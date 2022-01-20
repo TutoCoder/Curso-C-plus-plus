7a0d757549fb56832ea73bd92f19e0e809a1cd0b
Armando <armandoolivalopez@gmail.com>
2022-01-19 22:27:15 -0600

    We add the Session 01

---
+++ Sesión 01/holaMundo.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // Libreria, la entrada y salida de datos
+
+using namespace std; // p/ usar cin y cout
+
+int main(){ //funcion principal
+
+    cout << "Hola mundo"; // Imprimer el mensaje
+
+    return 0;
+}
---
+++ Sesión 01/variables.cpp
@@ -0,0 +1,26 @@
+#include <iostream> // Libreria, la entrada y salida de datos
+
+using namespace std; // p/ usar cin y cout
+
+int main(){ //funcion principal
+
+    // Variables - son un espacio en memoria
+    // Enteros - -infinito -1  || 0 || 1 - infinito
+    int numero = 50;
+
+    // Float - Flotantes // 10.5 // Lower Camel Case
+    float numeroFloat = 10.5;
+
+    // Double  4.1416
+    double numeroDecimal = 4.1416;
+
+    // char
+    char letra = 'T';
+
+    // string
+    string texto = "Armando Oliva";
+
+    cout << texto; //Imprimer el valor de la variable
+
+    return 0;
+}
